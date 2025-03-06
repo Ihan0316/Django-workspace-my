3075c3ef24c6ea6bf730561d425131f45f1063e2
“Ihan0316” <aijnjj12@gmail.com>
2025-03-06 15:25:47 +0900

    24/3/6 6교시 장고 설치, url 세팅

--- helloworld/helloworld/urls.py
+++ helloworld/helloworld/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 # from django.contrib import admin
 from django.urls import path
 
+from helloworld.views import main
+
 urlpatterns = [
     #    path("admin/", admin.site.urls),
+    path("", main),
 ]
