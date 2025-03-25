e90a51d87966d2aaecbc8491d2b0e26feec96bb2
“Ihan0316” <aijnjj12@gmail.com>
2025-03-25 16:47:11 +0900

    25/3/25 3교시 모델 flask 서버로 돌리기

--- FlaskProject-CNN-Test/app.py
+++ FlaskProject-CNN-Test/app.py
@@ -32,7 +32,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # ✅ YOLO 모델 로드
 # yolo_model = YOLO("model/best-busanit501-aqua.pt")
-yolo_model = YOLO("model/best_dogs_cats.pt")
+yolo_model = YOLO("model/best.pt")
 # app.config['SERVER_NAME'] = '10.100.201.87:5000'  # Flask 서버 주소와 포트 설정
 
 # ✅ 결과 저장 폴더 설정
